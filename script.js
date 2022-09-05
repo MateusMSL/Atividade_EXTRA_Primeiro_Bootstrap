2501bb22c91911110e140a8dfe6531b926835228
Mateus Meira <109548196+MateusMSL@users.noreply.github.com>
2022-09-05 15:14:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function clickMe1(){
+    alert("Voto para Link Majoras Mask confirmado!");
+}
+
+function clickMe2(){
+    alert("Voto para Link Twilight Princess confirmado!");
+}
+
+function clickMe3(){
+    alert("Voto para Link Ocarina of Time confirmado!");
+}
+
+function clickMe4(){
+    alert("Voto para Link Minish Cap confirmado!");
+}
